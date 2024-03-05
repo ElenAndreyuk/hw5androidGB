4552a6c7a29ee79119b4b9f8c73d7d174159d3d8
Elena <femtochka@yahoo.com>
2024-03-05 17:53:38 +0300

    hw5 revert

--- app/src/main/java/ru/elenandreyuk/quizzbygb/MainActivity.kt
+++ app/src/main/java/ru/elenandreyuk/quizzbygb/MainActivity.kt
@@ -2,14 +2,10 @@ package ru.elenandreyuk.quizzbygb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import ru.elenandreyuk.quizzbygb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
